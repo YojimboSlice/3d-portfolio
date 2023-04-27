26f1ed9d20cb3d56fc8c787ceabeb1c14b65a90a
YojimboSlice <jamesryanlan@gmail.com>
2023-04-26 22:31:02 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
@@ -7,44 +7,6 @@ import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({ name: '', email: '', message: '' });
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = e => {
-    const { name, value } = e.target;
-
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    setLoading(true);
-
-    {
-      /*try {
-      await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(new FormData(formRef.current)).toString(),
-      });
-
-      setLoading(false);
-      alert('Thank you. I will get back to you as soon as possible.');
-
-      setForm({
-        name: '',
-        email: '',
-        message: '',
-      });
-    } catch (error) {
-      setLoading(false);
-      console.error(error);
-      alert('Something went wrong.');
-    }*/
-    }
-  };
-
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
       <motion.div
@@ -53,11 +15,9 @@ const Contact = () => {
         <p className={styles.sectionsubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
         <form
-          ref={formRef}
-          onSubmit={handleSubmit}
           className='mt-12 flex flex-col gap-8'
           name='contact'
-          method='POST'
+          method='post'
           data-netlify='true'>
           <input
             type='hidden'
@@ -73,8 +33,6 @@ const Contact = () => {
             <input
               type='text'
               name='name'
-              value={form.name}
-              onChange={handleChange}
               placeholder='Enter name here'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-non font-medium'
             />
@@ -84,8 +42,6 @@ const Contact = () => {
             <input
               type='email'
               name='email'
-              value={form.email}
-              onChange={handleChange}
               placeholder='Enter email here'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-non font-medium'
             />
@@ -95,8 +51,6 @@ const Contact = () => {
             <textarea
               rows='7'
               name='message'
-              value={form.message}
-              onChange={handleChange}
               placeholder='Type message here'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-non font-medium'
             />
@@ -104,7 +58,7 @@ const Contact = () => {
           <button
             type='submit'
             className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'>
-            {loading ? 'Sending...' : 'Send'}
+            Send
           </button>
         </form>
       </motion.div>
