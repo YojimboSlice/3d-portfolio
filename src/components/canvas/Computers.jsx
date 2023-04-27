6916af075dc389d0718e928d947fffd77d2219e5
YojimboSlice <jamesryanlan@gmail.com>
2023-04-26 17:20:20 -0700

    Initial commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,103 @@
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
+import { Howl, Howler } from 'howler';
+import { guitarsound } from '../../assets';
+
+const Desktop = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
+  return (
+    <mesh>
+      <hemisphereLight
+        intensity={0.15}
+        groundColor='black'
+      />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.25]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const Guitar = ({ isMobile }) => {
+  const guitar = useGLTF('./guitar/acoustic_guitar.gltf');
+  Howler.volume(0.6);
+  const guitarSound = new Howl({
+    src: [guitarsound],
+  });
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleGuitarClick = () => {
+    setIsPlaying(!isPlaying);
+    if (!isPlaying) {
+      guitarSound.play();
+    } else {
+      guitarSound.pause();
+    }
+  };
+
+  return (
+    <mesh onClick={handleGuitarClick}>
+      <primitive
+        object={guitar.scene}
+        scale={isMobile ? 0.7 : 4.9}
+        position={isMobile ? [0, -3, -2.2] : [0.5, -2.9, 4.35]}
+        rotation={[1.6, 3.07, -1.5]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = event => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Desktop isMobile={isMobile} />
+        <Guitar isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
---
+++ src/constants/index.js
@@ -0,0 +1,204 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  crwnclothing,
+  expensestracker,
+  jobit,
+  tripguide,
+  threejs,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Web Developer',
+    icon: web,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'Content Creator',
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Three JS',
+    icon: threejs,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Web Developer',
+    company_name: '',
+    // icon: starbucks,
+    iconBg: '#383E56',
+    date: 'April 2022 - Present',
+    points: [
+      'Learning the fundamentals of JavaScript and how to use it in a web environment',
+      'Learn the fundamentals of HTML and CSS in order to build web applications',
+      'Building projects in JavaScript, such as a mock up banking app, to apply JavaScript knowledge',
+    ],
+  },
+  {
+    title: 'React Developer',
+    company_name: '',
+    // icon: tesla,
+    iconBg: '#E6DEDD',
+    date: 'Oct 2022 - Present',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Three.js Developer',
+    company_name: '',
+    // icon: tesla,
+    iconBg: '#E6DEDD',
+    date: 'April 2023 - Present',
+    points: [
+      'Learning the three.js library and various relevant technologies to create 3d websites',
+      'Implementing responsive design and ensuring cross-browser compatibility of the 3d models.',
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
+  },
+];
+
+const projects = [
+  {
+    name: 'Crwn Clothing',
+    description: `Web-based e-commerce platform for fashion. Users can browse for men and women's hats, jackets, and sneakers. Integrates with stripe API for payments and Google FireStore for data storage. Google Sign In also featured.`,
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'html',
+        color: 'orange-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: crwnclothing,
+    source_code_link: 'https://github.com/YojimboSlice/crwn-clothing',
+  },
+  {
+    name: 'Expenses Tracker',
+    description:
+      'Web application that enables users to track expenses, filter through expenses by year, and receive visual feedback in the form of a bar graph using the months for each bar.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'html',
+        color: 'orange-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: expensestracker,
+    source_code_link: 'https://github.com/YojimboSlice/expenses-tracker',
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
