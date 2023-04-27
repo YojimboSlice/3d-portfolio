d16698a976ddf2ae8a79543e825c02ff025b4530
YojimboSlice <jamesryanlan@gmail.com>
2023-04-27 01:12:58 -0700

    Test emailjs id's
    Temporary test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_f7a9yg9',
+        'template_vi2hbzj',
         {
           from_name: form.name,
           to_name: 'James Yan Lan',
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: 'jamesryanlan@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        '0yueb1DhurlGuHUxj',
       )
       .then(
         () => {
@@ -106,7 +106,7 @@ const Contact = () => {
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='What you do want to say?'
+              placeholder='What do you want to say?'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
