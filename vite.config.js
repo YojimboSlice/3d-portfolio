f4a0440fe3818789be05542a4c7a2df805ac19e3
YojimboSlice <jamesryanlan@gmail.com>
2023-04-28 15:38:27 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/3d-portfolio/',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          } else if (id.includes('src')) {
+            return 'app';
+          }
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['emailjs'],
+  },
 });
