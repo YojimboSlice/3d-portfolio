6929d7e1536aaf0a42ee2a3112d0c094ec6bcee0
YojimboSlice <jamesryanlan@gmail.com>
2023-04-28 17:56:35 -0700

    Update deploy

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default {
   plugins: [react()],
   base: '/3d-portfolio/',
-  build: {
-    define: {
-      'import.meta.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(
-        process.env.EMAILJS_SERVICE_ID,
-      ),
-      'import.meta.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(
-        process.env.EMAILJS_TEMPLATE_ID,
-      ),
-      'import.meta.env.VITE_EMAILJS_PUBLIC_KEY': JSON.stringify(
-        process.env.EMAILJS_PUBLIC_KEY,
-      ),
-    },
-  },
 };
